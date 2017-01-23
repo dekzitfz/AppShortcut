6d208fb75674ad0245aa1641035a9cea021dc12d
dekzitfz <dekz.itfz@gmail.com>
2017-01-23 21:26:48 +0800

    add dynamic shortcut in main activity

--- app/src/main/java/it/dekz/appshortcut/MainActivity.java
+++ app/src/main/java/it/dekz/appshortcut/MainActivity.java
@@ -1,7 +1,17 @@
 package it.dekz.appshortcut;
 
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //buat list untuk menampung shortcut
+        List<ShortcutInfo> shortcutInfos = new ArrayList<>();
+
+        //cek jika os menggunakan API 25
+        if (android.os.Build.VERSION.SDK_INT >= 25) {
+            //inisiasi ShortcutManager
+            ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+
+            //membuat object shortcutInfo
+            ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(this, "web_shortcut")
+                    .setShortLabel("dekzitfz.github.io")
+                    .setLongLabel("Visit dekzitfz github pages")
+                    .setIcon(Icon.createWithResource(this, R.drawable.ic_web))
+                    .setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("https://dekzitfz.github.io/")))
+                    .build();
+
+            //tambahkan objeck ke dalam list
+            shortcutInfos.add(shortcutInfo);
+
+            //set shortcut dinamis
+            shortcutManager.setDynamicShortcuts(shortcutInfos);
+        }
+
     }
 }
