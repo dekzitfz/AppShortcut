ea8c23ac87f34e39e59b6adcbbb1a867509bf57c
Adi Andrea <dekz.itfz@gmail.com>
2017-03-09 17:20:51 +0800

    add Calculator class to test

--- app/src/androidTest/java/it/dekz/appshortcut/UnitTestExample.java
+++ app/src/androidTest/java/it/dekz/appshortcut/UnitTestExample.java
@@ -11,9 +11,18 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class UnitTestExample extends TestCase {
-    @Test
+    /*@Test
     public void test2() throws Exception{
         MainActivity mainActivity = new MainActivity();
         assertEquals(1,mainActivity.cobatest());
+    }*/
+
+    @Test
+    public void test_add() throws Exception {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(3, 2);
+
+        assertEquals(5, result);
     }
 }
